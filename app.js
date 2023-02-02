f6ded6ae0a088d027f989f3fcf8d2b7e2833ee55
brandon3293 <brandona340@nycstudents.net>
2023-02-01 22:49:35 -0500

    add

--- app.js
+++ app.js
@@ -5,25 +5,28 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
 
 genre=["Pop", "Hip Hop", "Rap", "Classical", "Rock", "Jazz", "Blues"]
 genre=genre.sort();
 
 songs=[
     {id:1, name:'War Pigs', genre:'Heavy Metal'},
-    {id:2, name:'', genre:''},
-    {id:3, name:'', genre:''},
-    {id:4, name:'', genre:''}
+    {id:2, name:'War Pigs', genre:'War Pigs'},
+    {id:3, name:'War Pigs', genre:'War Pigs'},
+    {id:4, name:'War Pigs', genre:'War Pigs'}
 ]
+app.use(express.json());
 //get requests
-
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to Brandon\'s music app!')
+})
 
 //post requests
 
 
 //delete requests
+
+
 
 
 app.listen(500,()=>{
